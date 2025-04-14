430a38c563d7ddbb906eeb4e81a2427d0ee17d48
DagGiaKiet <giakietchatgpt@gmail.com>
2025-04-14 08:45:28 +0700

    ThemAnh

--- script.js
+++ script.js
@@ -2,5 +2,21 @@ document.getElementById('actionButton').addEventListener('click', function() {
     const members = ['Phùng Quốc Kiệt', 'Đặng Gia Kiệt', 'Hồ Hoàng Long'];
     const list = members.map(name => `<li>${name}</li>`).join('');
     document.getElementById('membersList').innerHTML = `<ul>${list}</ul>`;
-  });
-  
+
+    // Hiển thị hình ảnh
+    const image = document.getElementById('specificImage');
+    image.style.display = 'block';
+    
+});
+document.getElementById("actionButton").addEventListener("click", function() {
+  const image = document.getElementById("DangGiaKiet");
+  image.style.display = "block"; // Hiển thị hình ảnh
+});
+document.getElementById("actionButton").addEventListener("click", function() {
+  const image = document.getElementById("QuocKiet");
+  image.style.display = "block"; // Hiển thị hình ảnh
+});
+document.getElementById("actionButton").addEventListener("click", function() {
+  const image = document.getElementById("HoangLong");
+  image.style.display = "block"; // Hiển thị hình ảnh
+});
